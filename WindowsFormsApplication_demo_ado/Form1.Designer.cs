565b7ab6fd16ed1f03cc954c777e5beb37a08506
DoVuDung <vudungoik2016@gmail.com>
2021-06-15 22:50:06 +0700

    demo from teacher

---
+++ WindowsFormsApplication_demo_ado/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace WindowsFormsApplication_demo_ado
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column_lop = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewImageColumn();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(57, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(164, 30);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column_lop,
+            this.Column4});
+            this.dataGridView1.Location = new System.Drawing.Point(34, 121);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(545, 150);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Column1
+            // 
+            this.Column1.DataPropertyName = "MASV";
+            this.Column1.HeaderText = "Mã SV";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.DataPropertyName = "HOTEN";
+            this.Column2.HeaderText = "Họ Tên";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.DataPropertyName = "NGAYSINH";
+            dataGridViewCellStyle1.Format = "dd/MM/yyyy";
+            dataGridViewCellStyle1.NullValue = null;
+            this.Column3.DefaultCellStyle = dataGridViewCellStyle1;
+            this.Column3.HeaderText = "Ngày sinh";
+            this.Column3.Name = "Column3";
+            // 
+            // Column_lop
+            // 
+            this.Column_lop.DataPropertyName = "MALOP";
+            this.Column_lop.HeaderText = "Lớp";
+            this.Column_lop.Name = "Column_lop";
+            this.Column_lop.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Column_lop.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // Column4
+            // 
+            this.Column4.DataPropertyName = "HINHANH";
+            this.Column4.HeaderText = "Ảnh";
+            this.Column4.ImageLayout = System.Windows.Forms.DataGridViewImageCellLayout.Zoom;
+            this.Column4.Name = "Column4";
+            this.Column4.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Column4.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(34, 94);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(273, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(421, 455);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(95, 376);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(95, 402);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(95, 428);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(95, 498);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "dd/MM/yyyy";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(95, 454);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(100, 20);
+            this.dateTimePicker1.TabIndex = 7;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(211, 376);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(101, 98);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(318, 451);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(211, 497);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(504, 338);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 11;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(625, 580);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewComboBoxColumn Column_lop;
+        private System.Windows.Forms.DataGridViewImageColumn Column4;
+    }
+}
+
---
+++ WindowsFormsApplication_demo_ado/Form1.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace WindowsFormsApplication_demo_ado
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //string constr = @"Data Source=DESKTOP-5U0CT07\SQL2012; Initial Catalog=DEMO; Uid=sa; Pwd=0123";
+            //SqlConnection con = new SqlConnection(constr);
+            //if (con.State != ConnectionState.Open)
+            //    con.Open();
+            ////
+            ////SqlCommand cmd = new SqlCommand("insert into NHANVIEN (MASO,HOTEN) values ('NV03','CCC')", con);
+            ////SqlCommand cmd = new SqlCommand("update NHANVIEN set HOTEN='Nguyen Van C' where MASO='NV03'", con);
+            ////SqlCommand cmd = new SqlCommand("delete from NHANVIEN where MASO='NV03'", con);
+            ////int i = cmd.ExecuteNonQuery();
+
+            ////SqlCommand cmd = new SqlCommand("select top 1 HOTEN from NHANVIEN where MASO='NV02'", con);
+            ////object o = cmd.ExecuteScalar();
+
+            //SqlCommand cmd = new SqlCommand("select * from NHANVIEN", con);
+            //SqlDataReader reader = cmd.ExecuteReader();
+
+            //if (reader.HasRows)
+            //{
+            //    while (reader.Read())
+            //    {
+            //        //MessageBox.Show(reader.GetString(0) + " - " + reader.GetString(1));
+            //        MessageBox.Show(reader["MASO"] + " - " + reader["HOTEN"]);
+            //    }
+            //    reader.Close();
+            //}
+
+            ////
+            //con.Close();
+
+            string constr = @"Data Source=.\SQLEXPRESS;AttachDbFilename=C:\Users\huynh\Desktop\WindowsFormsApplication_demo_ado\WindowsFormsApplication_demo_ado\STUDENT.mdf;Integrated Security=True;User Instance=True";
+            SqlConnection con = new SqlConnection(constr);
+            SqlDataAdapter da = new SqlDataAdapter("select * from LOP", con);
+
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+
+            comboBox1.DataSource = ds.Tables[0];
+            comboBox1.DisplayMember = "TENLOP";
+            comboBox1.ValueMember = "MALOP";
+
+            Column_lop.DataSource = ds.Tables[0];
+            Column_lop.DisplayMember = "TENLOP";
+            Column_lop.ValueMember = "MALOP";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //string constr = @"Data Source=.\SQLEXPRESS;AttachDbFilename=C:\Users\huynh\Desktop\WindowsFormsApplication_demo_ado\WindowsFormsApplication_demo_ado\STUDENT.mdf;Integrated Security=True;User Instance=True";
+
+            string constr = System.Configuration.ConfigurationManager.ConnectionStrings["conStr"].ConnectionString;
+
+            SqlConnection con = new SqlConnection(constr);
+            if (con.State != ConnectionState.Open)
+                con.Open();
+
+            SqlCommand cmd = new SqlCommand("select * from SINHVIEN", con);
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    MessageBox.Show(reader["MASV"] + " - " + reader["HOTEN"] + " - " + reader["MALOP"]);
+                }
+                reader.Close();
+            }
+
+            //
+            con.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string constr = System.Configuration.ConfigurationManager.ConnectionStrings["conStr"].ConnectionString;
+            SqlConnection con = new SqlConnection(constr);
+
+            SqlDataAdapter da = new SqlDataAdapter("select * from SINHVIEN select * from LOP", con);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+
+            dataGridView1.DataSource = ds.Tables[0];
+            comboBox1.DataSource = ds.Tables[1];
+            comboBox1.DisplayMember = "TENLOP";
+            comboBox1.ValueMember = "MALOP";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //string constr = System.Configuration.ConfigurationManager.ConnectionStrings["conStr"].ConnectionString;
+            string constr = @"Data Source=.\SQLEXPRESS;AttachDbFilename=C:\Users\huynh\Desktop\WindowsFormsApplication_demo_ado\WindowsFormsApplication_demo_ado\STUDENT.mdf;Integrated Security=True;User Instance=True";
+            SqlConnection con = new SqlConnection(constr);
+
+            SqlDataAdapter da = new SqlDataAdapter("select * from SINHVIEN", con);
+            SqlCommandBuilder builder = new SqlCommandBuilder(da);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+
+            ds.Tables[0].Rows[0]["HOTEN"] = "AAA";
+            DataRow r = ds.Tables[0].NewRow();
+            r["MASV"] = "SV02";
+            r["HOTEN"] = "CCC";
+            r["MALOP"] = "TTT";
+            ds.Tables[0].Rows.Add(r);
+
+            da.Update(ds);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string constr = @"Data Source=.\SQLEXPRESS;AttachDbFilename=C:\Users\huynh\Desktop\WindowsFormsApplication_demo_ado\WindowsFormsApplication_demo_ado\STUDENT.mdf;Integrated Security=True;User Instance=True";
+            SqlConnection con = new SqlConnection(constr);
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "insert into SINHVIEN (MASV, HOTEN, NGAYSINH, MALOP, HINHANH) values (@MASV, @HOTEN, @NGAYSINH, @MALOP, @HINHANH)";
+            cmd.Parameters.Add(new SqlParameter("@MASV", textBox1.Text));
+            cmd.Parameters.Add(new SqlParameter("@HOTEN", textBox2.Text));
+            cmd.Parameters.Add(new SqlParameter("@NGAYSINH", dateTimePicker1.Value.Date));
+            cmd.Parameters.Add(new SqlParameter("@MALOP", textBox3.Text));
+
+            SqlParameter para = new SqlParameter("@HINHANH", SqlDbType.Image);
+            para.Value = ImageToByte(pictureBox1.Image);
+            cmd.Parameters.Add(para);
+
+            cmd.Connection = con;
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+        public Image ByteToImage(byte[] byteArray)
+        {
+            MemoryStream ms = new MemoryStream(byteArray);
+            Image returnImage = Image.FromStream(ms);
+            return returnImage;
+        }
+
+        public byte[] ImageToByte(Image image)
+        {
+            MemoryStream ms = new MemoryStream();
+            image.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+            return ms.ToArray();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog f = new OpenFileDialog();
+            f.Filter = "Anh jpg|*.jpg";
+            if (f.ShowDialog() == DialogResult.OK)
+                pictureBox1.Image = Image.FromFile(f.FileName);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string constr = @"Data Source=.\SQLEXPRESS;AttachDbFilename=C:\Users\huynh\Desktop\WindowsFormsApplication_demo_ado\WindowsFormsApplication_demo_ado\STUDENT.mdf;Integrated Security=True;User Instance=True";
+            SqlConnection con = new SqlConnection(constr);
+
+            //SqlDataAdapter da = new SqlDataAdapter("select * from SINHVIEN where MASV='SV07'", con);
+            SqlDataAdapter da = new SqlDataAdapter();
+
+            SqlCommand cmd = new SqlCommand("getSinhvien", con);
+            cmd.Parameters.Add(new SqlParameter("@MASV", "SV07"));
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            da.SelectCommand = cmd;
+
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+                MessageBox.Show(dt.Rows[i]["MASV"] + "");
+
+            foreach (DataRow r in dt.Rows) MessageBox.Show(r["MASV"] + "");
+
+            DataRow dr = dt.NewRow();
+            dr["MASV"] = "CCC";
+            //
+            dt.Rows.Add(dr);
+
+
+
+            if (dt.Rows.Count > 0)
+            {
+                textBox1.Text = dt.Rows[0]["MASV"] + "";
+                textBox2.Text = dt.Rows[0]["HOTEN"] + "";
+                textBox3.Text = dt.Rows[0]["MALOP"] + "";
+                dateTimePicker1.Value = dt.Rows[0]["NGAYSINH"] + "" != "" ? Convert.ToDateTime(dt.Rows[0]["NGAYSINH"]) : DateTime.Now;
+                pictureBox1.Image = dt.Rows[0]["HINHANH"] + "" != "" ? ByteToImage((byte[])dt.Rows[0]["HINHANH"]) : null;
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string constr = @"Data Source=.\SQLEXPRESS;AttachDbFilename=C:\Users\huynh\Desktop\WindowsFormsApplication_demo_ado\WindowsFormsApplication_demo_ado\STUDENT.mdf;Integrated Security=True;User Instance=True";
+            SqlConnection con = new SqlConnection(constr);
+            SqlDataAdapter da = new SqlDataAdapter("select * from SINHVIEN select * from LOP", con);
+
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+
+            dataGridView1.DataSource = ds.Tables[0];
+
+            comboBox1.DataSource = ds.Tables[1];
+            comboBox1.DisplayMember = "TENLOP";
+            comboBox1.ValueMember = "MALOP";
+
+            //DataView dv = dt.DefaultView;
+            //dv.Sort = "HOTEN";
+            //dv.RowFilter = "MALOP='TTT'";
+            //object o = dt.Compute("count(MASV)", "MALOP='TTT'");
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedValue + "" == "") return;
+
+            string constr = @"Data Source=.\SQLEXPRESS;AttachDbFilename=C:\Users\huynh\Desktop\WindowsFormsApplication_demo_ado\WindowsFormsApplication_demo_ado\STUDENT.mdf;Integrated Security=True;User Instance=True";
+            SqlConnection con = new SqlConnection(constr);
+            SqlDataAdapter da = new SqlDataAdapter("select * from SINHVIEN where MALOP='" + comboBox1.SelectedValue + "'", con);
+
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.DataSource = ds.Tables[0];
+           
+
+            textBox1.DataBindings.Clear();
+            textBox2.DataBindings.Clear();
+            textBox3.DataBindings.Clear();
+            dateTimePicker1.DataBindings.Clear();
+            //pictureBox1.DataBindings.Clear();
+
+            textBox1.DataBindings.Add("Text", ds.Tables[0], "MASV");
+            textBox2.DataBindings.Add("Text", ds.Tables[0], "HOTEN");
+            textBox3.DataBindings.Add("Text", ds.Tables[0], "MALOP");
+            dateTimePicker1.DataBindings.Add("Text", ds.Tables[0], "NGAYSINH");
+            //pictureBox1.DataBindings.Add("Image", ds.Tables[0], "HINHANH");
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
